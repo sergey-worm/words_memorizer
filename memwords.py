038477a6472301720df89db04f671bfb11e7934b
Sergey Worm <sergey.worm@gmail.com>
2019-05-28 18:52:09 +0300

    Added new words.

--- memwords.py
+++ memwords.py
@@ -5,6 +5,8 @@ import os
 import random
 import re
 
+DIRECTION = 'random'
+
 OK = 0
 WRONG = 0
 
@@ -19,36 +21,54 @@ class colors:
 def process_line(line):
 	substrs = re.split(':', line)
 	# ask one word from left/right and expect answer from right/left
-	ask_substr = random.randint(0, 1)
-	words_ask = re.split(',', substrs[ask_substr])
-	words_exp = re.split(',', substrs[not ask_substr])
-	words_ask = [item.strip() for item in words_ask]
-	words_exp = [item.strip() for item in words_exp]
+	ask_substr = -1
+	global DIRECTION
+	if DIRECTION == 'random':
+		ask_substr = random.randint(0, 1)
+	if DIRECTION == 'forward':
+		ask_substr = 0
+	if DIRECTION == 'backward':
+		ask_substr = 1
+	words_descr = ''
+	if len(substrs) > 2:
+		words_descr = substrs[2].strip()
+	words_ask   = re.split(',', substrs[ask_substr])
+	words_exp   = re.split(',', substrs[not ask_substr])
+	words_ask   = [item.strip() for item in words_ask]
+	words_exp   = [item.strip() for item in words_exp]
 	# ask
 	a = random.randint(0, len(words_ask)-1)
 	print("ask    |  {}".format(words_ask[a]))
+	global OK
+	global WRONG
 	for i in range(0, 3):
 		print("answer |  ".format(words_ask[a]), end="")
 		answer = input()
 		# check answer
 		if answer in words_exp:
-			global OK
 			OK += 1
-			print("       |", colors.green, "ok - {}".format(OK), colors.endc)
+			print("       |", colors.green, "ok:  {}  {}%  {}".format(OK,
+				round(100*OK/(OK+WRONG)), words_exp), colors.endc)
+			print("       |", colors.blue, "{}".format(words_descr), colors.endc)
 			break
 		if answer == 'q':
 			print("       |", colors.blue, "exit", colors.endc)
 			sys.exit()
 		else:
-			global WRONG
 			WRONG += 1
-			print("       |", colors.yellow, "wrong - {}".format(WRONG), colors.endc)
+			print("       |", colors.yellow, "wrong:  {}  {}%".format(WRONG,
+				round(100*WRONG/(OK+WRONG))), colors.endc)
 			if i == 2:
 				print("       |", colors.red, "failed:  {}".format(words_exp), colors.endc)
+				print("       |", colors.blue, "{}".format(words_descr), colors.endc)
 	print("-------+------------------------------")
 
 def main():
-	filepath = sys.argv[1]
+	filepath  = sys.argv[1]
+
+	global DIRECTION
+	if len(sys.argv) > 2:
+		DIRECTION = sys.argv[2]
 
 	if not os.path.isfile(filepath):
 		print("File path {} does not exist. Exiting.".format(filepath))
